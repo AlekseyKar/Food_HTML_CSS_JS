2e4a09e0d647e9a444b7899c142a5c2888c5accd
AlekseyKar <alekseykar92@gmail.com>
2022-10-31 20:12:10 +0300

    Pre: add new operator ?.

---
+++ preview/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const box = document.querySelector('.box');
+const block = document.querySelector('.block');
+
+console.log(block)
+// проверка операторм ? на наличие блока  
+console.log(block?.textContent);
+
+console.log(1+2);
