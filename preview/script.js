a3b90664025f0e14ab6329cb3248670a5b13820d
AlekseyKar <alekseykar92@gmail.com>
2022-11-01 14:16:07 +0300

    pre: new set collect

--- preview/script.js
+++ preview/script.js
@@ -1,15 +1,24 @@
 'use strict';
 
-const shops = [
-  {rice: 200},
-  {rice: 300},
-  {rice: 400},
-];
-const budget = [5000, 12222, 15555];
+// const shops = [
+//   {rice: 200},
+//   {rice: 300},
+//   {rice: 400},
+// ];
+// const budget = [5000, 12222, 15555];
 
-const map = new Map();
+// const map = new Map();
 
-shops.forEach((shop,i) => { 
-  map.set(shop, budget[i]);
-})
-console.log(map);
+// shops.forEach((shop,i) => { 
+//   map.set(shop, budget[i]);
+// })
+// console.log(map);
+
+
+const arr = ['alex', 'bob', 'stive', 'bob'];
+
+const set = new Set(arr);
+
+set.add('mike');
+
+console.log(set);
