9b403f911df197ff0830e6871b042b37f1aa8516
AlekseyKar <alekseykar92@gmail.com>
2022-11-02 12:23:22 +0300

    s-1: add and remove class

--- js_methods/script.js
+++ js_methods/script.js
@@ -11,5 +11,11 @@ const btns = document.querySelectorAll('button');
 
 // add and remove class 
 btns[0].addEventListener('click', () => {
-  
-})
+  // if (!btns[1].classList.contains("red")) {
+  //   btns[1].classList.add("red");
+  // }else {
+  //   btns[1].classList.remove("red");
+  // }
+  btns[1].classList.toggle("red");
+});
+console.log(btns[0].className);
